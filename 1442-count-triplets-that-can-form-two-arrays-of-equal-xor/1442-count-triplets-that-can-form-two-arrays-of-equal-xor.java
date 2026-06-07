2823d9db96b1449bc32966261535744f4294c92b
prakharmuley <75474426+prakharmuley@users.noreply.github.com>
2026-06-07 16:48:50 +0530

    Time: 2 ms (79.32%), Space: 43.6 MB (6.48%) - LeetHub

--- 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.java
+++ 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.java
@@ -1,23 +1,47 @@
+// class Solution {
+//     public int countTriplets(int[] arr) {
+//         int ans=0;
+//         int n=arr.length;
+//         int xor=0;
+//         for(int i=0;i<n;i++)
+//         {
+//             xor=arr[i];
+//             for(int j=i+1;j<n;j++)
+//             {
+//                 xor^=arr[j];
+//                 if(xor==0)
+//                 {
+//                     ans+=(j-i);
+//                 }
+//             }
+            
+//         }
+//         return ans;
+//     }
+// }
+
 class Solution {
     public int countTriplets(int[] arr) {
-        int ans=0;
-        int n=arr.length;
-        int xor=0;
-        for(int i=0;i<n;i++)
+     int ans=0;
+     int xor=0;
+     final int n=arr.length;
+     Map<Integer,Integer> count=new HashMap<>();
+     Map<Integer,Integer> sum=new HashMap<>();
+     count.put(0,1);
+     sum.put(0,0);
+     for(int i=0;i<n;i++)
+     {
+        xor^=arr[i];
+        if(count.containsKey(xor))
         {
-            xor=arr[i];
-            for(int j=i+1;j<n;j++)
-            {
-                xor^=arr[j];
-                if(xor==0)
-                {
-                    ans+=(j-i);
-                }
-            }
-            
+            ans+=count.get(xor)*i-sum.get(xor);
         }
-        return ans;
-    }
+        count.put(xor,count.getOrDefault(xor,0)+1);
+        sum.put(xor,sum.getOrDefault(xor,0)+i+1);
+
+     }
+     return ans;
+}
 }
 /*
 2 3 1 6 7
