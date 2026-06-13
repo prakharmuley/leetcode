cf003a7ccb6cad829d99b3f5278397bf2a97e865
prakharmuley <75474426+prakharmuley@users.noreply.github.com>
2026-06-14 00:50:01 +0530

    Time: 13 ms (79.55%), Space: 91.1 MB (12.27%) - LeetHub

--- 2536-increment-submatrices-by-one/2536-increment-submatrices-by-one.java
+++ 2536-increment-submatrices-by-one/2536-increment-submatrices-by-one.java
@@ -1,29 +1,71 @@
 class Solution {
     public int[][] rangeAddQueries(int n, int[][] queries) {
         int[][] ans=new int[n][n];
-        for(int[] q: queries)
+        // for(int[] q: queries)
+        // {
+        //     int r1=q[0];
+        //     int c1=q[1];
+        //     int r2=q[2];
+        //     int c2=q[3];
+        //     for(int i=r1;i<=r2;i++)
+        //     {
+        //         ans[i][c1]+=1;
+        //         if(c2+1<n)
+        //         {
+        //             ans[i][c2+1]-=1;
+        //         }
+        //     }
+           
+        // }
+        //  for(int i=0;i<n;i++)
+        //     {
+        //         for(int j=1;j<n;j++)
+        //         {
+        //             ans[i][j]+=ans[i][j-1];
+        //         }
+        //     }
+        //     return ans;
+
+        //More optimization now
+        int[][] d=new int[n+1][n+1];
+         for(int[] q: queries)
         {
             int r1=q[0];
             int c1=q[1];
             int r2=q[2];
             int c2=q[3];
-            for(int i=r1;i<=r2;i++)
-            {
-                ans[i][c1]+=1;
-                if(c2+1<n)
-                {
-                    ans[i][c2+1]-=1;
-                }
-            }
-           
+            d[r1][c1]++;
+            d[r1][c2+1]--;
+            d[r2+1][c1]--;
+            d[r2+1][c2+1]++;
         }
-         for(int i=0;i<n;i++)
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
             {
-                for(int j=1;j<n;j++)
+                int top=0,left=0,cmn=0;
+                if(i>0)
                 {
-                    ans[i][j]+=ans[i][j-1];
+                    top=ans[i-1][j];
                 }
+                if(j>0)
+                {
+                  left=ans[i][j-1];
+                }
+                if(i>0&&j>0)
+                {
+                  cmn=ans[i-1][j-1];
+                }
+                ans[i][j]=d[i][j]+top+left-cmn;
+
             }
-            return ans;
+        }
+        return ans;
+
+
+
+
+
+
     }
 }
