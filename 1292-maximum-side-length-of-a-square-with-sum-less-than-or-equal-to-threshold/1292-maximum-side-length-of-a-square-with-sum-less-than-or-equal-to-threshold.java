d5dbf793383c5a8b73be14ef57a798a51735feff
prakharmuley <75474426+prakharmuley@users.noreply.github.com>
2026-06-14 19:49:07 +0530

    Time: 6 ms (98.89%), Space: 63.2 MB (45.96%) - LeetHub

--- 1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.java
+++ 1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.java
@@ -1,59 +1,39 @@
 class Solution {
-    public boolean isValid(int sr, int sc, int size, int[][] row, int[][] col, int[][] arr, int t)
-    {
-        int sum=row[sr][sc+size-1]-(sc>0?row[sr][sc-1]:0);
-        if(sum>t) return false;
-        int s1=0;
-        for(int i=sr;i<sr+size;i++)
-        {
-            s1=s1+row[i][sc+size-1]-(sc>0?row[i][sc-1]:0);
-        }
-         if(s1>t) return false;
-         int s2=0;
-        for(int i=sc;i<sc+size;i++)
-        {
-            s2=s2+col[sr+size-1][i]-(sr>0?col[sr-1][i]:0);
-        }
-         if(s2>t) return false;
-         return true;
-    }
     public int maxSideLength(int[][] mat, int threshold) {
-        int m=mat.length;
-        int n=mat[0].length;
-        int[][] row=new int[m][n];
-        int[][] col=new int[m][n];
-        for(int i=0;i<m;i++)
-        {
-            row[i][0]=mat[i][0];
-            for(int j=1;j<n;j++)
-            {
-                row[i][j]=row[i][j-1]+mat[i][j];
+        int m = mat.length;
+        int n = mat[0].length;
+
+        int[][] pre = new int[m + 1][n + 1];
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                pre[i][j] = pre[i - 1][j]
+                          + pre[i][j - 1]
+                          - pre[i - 1][j - 1]
+                          + mat[i - 1][j - 1];
             }
         }
-        for(int i=0;i<n;i++)
-        {
-            col[0][i]=mat[0][i];
-            for(int j=1;j<m;j++)
-            {
-                col[j][i]=col[j-1][i]+mat[j][i];
-            }
-        }
-        int ans=0;
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                int size=Math.min(m-i,n-j);
-                for(int sz=size;sz>ans;sz--)
-                {
-                    if(isValid(i,j,sz,row,col,mat,threshold))
-                    {
-                        ans=sz;
-                        break;
+
+        int ans = 0;
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i >= ans + 1 && j >= ans + 1) {
+                    int r1 = i - ans - 1;
+                    int c1 = j - ans - 1;
+                    int sum = pre[i][j]
+                            - pre[r1][j]
+                            - pre[i][c1]
+                            + pre[r1][c1];
+
+                    if (sum <= threshold) {
+                        ans++;
                     }
                 }
             }
         }
+
         return ans;
     }
 }
+
