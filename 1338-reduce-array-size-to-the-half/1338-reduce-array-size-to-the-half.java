c561c9bef1c6abfb025a8db8ea7b902dc5e36f68
prakharmuley <75474426+prakharmuley@users.noreply.github.com>
2026-07-03 01:41:20 +0530

    Time: 35 ms (83.41%), Space: 82.3 MB (91.94%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
@@ -1,29 +1,52 @@
+// class Solution {
+//     public int minSetSize(int[] arr) {
+//         int half=arr.length/2;
+//         int n=arr.length;
+//         PriorityQueue<Map.Entry<Integer,Integer>> pq=new PriorityQueue<>((a,b)->b.getValue()-a.getValue());
+//         Map<Integer,Integer> mp=new HashMap<>();
+//         for(int i=0;i<arr.length;i++) mp.put(arr[i],mp.getOrDefault(arr[i],0)+1);
+//         pq.addAll(mp.entrySet());
+//         int ans=0;
+//         while(!pq.isEmpty())
+//         {
+//             Map.Entry e=pq.poll();
+//             int key=(int)e.getKey();
+//             int val=(int)e.getValue();
+//             if(n-val<=half)
+//             {
+//                 ans++;
+//                 break;
+//             }
+//             else
+//             {
+//                 n-=val;
+//                 ans++;
+//             }
+//         }
+
+//         return ans;
+//     }
+// }
 class Solution {
     public int minSetSize(int[] arr) {
-        int half=arr.length/2;
-        int n=arr.length;
-        PriorityQueue<Map.Entry<Integer,Integer>> pq=new PriorityQueue<>((a,b)->b.getValue()-a.getValue());
-        Map<Integer,Integer> mp=new HashMap<>();
-        for(int i=0;i<arr.length;i++) mp.put(arr[i],mp.getOrDefault(arr[i],0)+1);
-        pq.addAll(mp.entrySet());
-        int ans=0;
-        while(!pq.isEmpty())
-        {
-            Map.Entry e=pq.poll();
-            int key=(int)e.getKey();
-            int val=(int)e.getValue();
-            if(n-val<=half)
-            {
-                ans++;
-                break;
-            }
-            else
-            {
-                n-=val;
-                ans++;
-            }
+        int len = arr.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int maxFreq=0;
+        for(int num : arr ){
+            map.put(num, map.getOrDefault(num, 0)+1);
+            maxFreq = Math.max(maxFreq, map.get(num));
         }
-
-        return ans;
+        int[] freqArr = new int[maxFreq+1];
+        for(int freq : map.values()){
+            freqArr[freq]++;
+        }
+        int res=0, half=len/2, removed=0, curFreq=maxFreq;
+        while(removed<half){
+            while(freqArr[curFreq]==0) curFreq--;
+            freqArr[curFreq]--;
+            removed+=curFreq;
+            res++;
+        }
+        return res;
     }
-}
+} 
