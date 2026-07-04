916afb94bbb6f09db180314d075e69eb309382f0
prakharmuley <75474426+prakharmuley@users.noreply.github.com>
2026-07-05 01:20:17 +0530

    Time: 1 ms (97.48%), Space: 42.8 MB (90.53%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.java
+++ 0767-reorganize-string/0767-reorganize-string.java
@@ -1,51 +1,37 @@
 class Solution {
     public String reorganizeString(String s) {
-        int h[]=new int[26];
-        int l=s.length();
-        StringBuilder sb=new StringBuilder();
-        Pair p[]=new Pair[26];
-        for(char c: s.toCharArray())  h[c-'a']++;
-        PriorityQueue<Pair>pq=new PriorityQueue<>((a,b)->b.v-a.v);
-        for(int i=0;i<26;i++) p[i]=new Pair((char)('a'+i),h[i]);
-        for(Pair pp: p) {if(pp.v>0) pq.offer(pp);}
-        if(pq.peek().v>(s.length()+1)/2) return "";
-        while(pq.size()>1)
-        {
-            Pair pp1=pq.poll();
-            Pair pp2=pq.poll();
-            char ch1=pp1.c;
-            int val1=pp1.v;
-            char ch2=pp2.c;
-            int val2=pp2.v;
-            sb.append(ch1);
-            sb.append(ch2);
-            pp1.v--;
-            pp2.v--;
-            
-            if(pp1.v>0)
-            {
-                pq.offer(pp1);
-            }
-            
-             if(pp2.v>0)
-            {
-                pq.offer(pp2);
+        int n = s.length();
+        int[] freq = new int[26];
+
+        int max = 0, idx = 0;
+        for (char c : s.toCharArray()) {
+            freq[c - 'a']++;
+            if (freq[c - 'a'] > max) {
+                max = freq[c - 'a'];
+                idx = c - 'a';
             }
         }
-        if(!pq.isEmpty())
-        {
-            Pair pp=pq.poll();
-            sb.append(pp.c);
+
+        if (max > (n + 1) / 2)
+            return "";
+
+        char[] ans = new char[n];
+        int i = 0;
+
+        while (freq[idx]-- > 0) {
+            ans[i] = (char) ('a' + idx);
+            i += 2;
         }
-        return sb.toString();
-    }
-}
-class Pair{
-    char c;
-    int v;
-    public Pair(char c, int v)
-    {
-        this.c=c;
-        this.v=v;
+
+        for (int j = 0; j < 26; j++) {
+            while (freq[j]-- > 0) {
+                if (i >= n)
+                    i = 1;
+                ans[i] = (char) ('a' + j);
+                i += 2;
+            }
+        }
+
+        return new String(ans);
     }
 }
