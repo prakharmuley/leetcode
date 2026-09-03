8c55bf96188d3fa327f3852ef0857bb87aa42466
prakharmuley <75474426+prakharmuley@users.noreply.github.com>
2026-09-03 23:28:01 +0530

    Time: 2 ms (64%), Space: 46.5 MB (38.25%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -14,24 +14,23 @@
  * }
  */
 class Solution {
-    int n;
-    public  TreeNode build(int in_st,int in_end,int[] in, int pre_st,int pre_end, int[] pre, Map<Integer,Integer> mp)
+    
+    private TreeNode dfs(int[] in, int in_st, int in_end, int[] pre, int pre_st, int pre_end, Map<Integer,Integer> mp)
     {
-      if(pre_st>pre_end) return null;
-      int idx=mp.get(pre[pre_st]);
-      int lsz=idx-in_st;
-      TreeNode root=new TreeNode(pre[pre_st]);
-      root.left=build(in_st,idx-1,in, pre_st+1, pre_st+lsz, pre,mp);
-      root.right=build(idx+1,in_end,in,pre_st+lsz+1,pre_end,pre,mp);
-      return root;
+         if(in_st>in_end||pre_st>pre_end) return null;
+         int idx=mp.get(pre[pre_st]);
+         TreeNode root=new TreeNode(pre[pre_st]);
+         int dist=idx-in_st;
+         root.left=dfs(in,in_st,idx-1,pre,pre_st+1,pre_st+dist,mp);
+         root.right=dfs(in,idx+1,in_end,pre,pre_st+dist+1,pre_end,mp);
+         return root;
+        
     }
     public TreeNode buildTree(int[] preorder, int[] inorder) {
+        int n=inorder.length;
         Map<Integer,Integer> mp=new HashMap<>();
-        this.n=inorder.length;
-        for(int i=0;i<n;i++)
-        {
-            mp.put(inorder[i],i);
-        }
-        return build(0,n-1,inorder,0,n-1,preorder,mp);
+        for(int i=0;i<n;i++) mp.put(inorder[i],i);
+        return dfs(inorder,0,n-1,preorder,0,n-1, mp);
+        
     }
 }
