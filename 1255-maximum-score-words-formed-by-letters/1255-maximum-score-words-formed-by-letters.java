40fea14e670f470082d48ddd61ba09396692f50b
prakharmuley <75474426+prakharmuley@users.noreply.github.com>
2026-09-05 19:04:26 +0530

    Time: 1 ms (89.86%), Space: 43.8 MB (41.79%) - LeetHub

--- 1255-maximum-score-words-formed-by-letters/1255-maximum-score-words-formed-by-letters.java
+++ 1255-maximum-score-words-formed-by-letters/1255-maximum-score-words-formed-by-letters.java
@@ -1,68 +1,53 @@
 class Solution {
     int ans=0;
-    public void dfs(int idx, int n,String[] words, boolean[] vis, int[] freq, int[] score, int[] a)
-    {
-         
-         ans=Math.max(ans,a[0]);
-         for(int i=idx;i<n;i++)
-         {
-            if(!vis[i])
-            {
-                boolean f=true;
-                String s=words[i];
-                int[] mark=new int[26];
-                for(int j=0;j<s.length();j++)
-                {
-                    mark[s.charAt(j)-'a']++;
-                    if(mark[s.charAt(j)-'a']>freq[s.charAt(j)-'a'])
-                    {
-                        f=false;
-                        break;
-                    }
-                                
-                }
 
-               
+    public void dfs(int idx, int n, String[] words, int[] freq,
+                    int[] score, int current) {
 
-                if(!f)
-                {
-                    continue;
-                }
-                 for(int j=0;j<s.length();j++)
-                  freq[s.charAt(j)-'a']--;
+        ans=Math.max(ans,current);
 
-                vis[i]=true;
-                for(int j=0;j<s.length();j++)
-                {
-                    a[0]+=score[s.charAt(j)-'a'];
+        for(int i=idx;i<n;i++) {
+            String s=words[i];
+            int[] mark=new int[26];
+            boolean f=true;
+
+            for(int j=0;j<s.length();j++) {
+                int x=s.charAt(j)-'a';
+                mark[x]++;
+
+                if(mark[x]>freq[x]) {
+                    f=false;
+                    break;
                 }
-                
-                
-                dfs(i,n,words,vis,freq,score,a);
-                 for(int j=0;j<s.length();j++)
-                {
-                    freq[s.charAt(j)-'a']++;
-                    a[0]-=score[s.charAt(j)-'a'];
-                }
-                vis[i]=false;
             }
-         }
-    }
-    public int maxScoreWords(String[] words, char[] letters, int[] score) {
-        int[] freqq=new int[26];
-        for(char c: letters) freqq[c-'a']++;
 
-        for(int i=0;i<words.length;i++)
-        {
-            boolean[] vis=new boolean[words.length];
-            int[] freq=freqq.clone();
-            int[] a=new int[1];
-            dfs(i,words.length,words,vis,freq,score,a);
-            System.out.print(a[0]+ " ---->"+i+"\n");
-           // ans=Math.max(ans,a[0]);
+            if(!f)
+                continue;
 
+            for(int j=0;j<s.length();j++) {
+                int x=s.charAt(j)-'a';
+                freq[x]--;
+                current+=score[x];
+            }
+
+            dfs(i+1,n,words,freq,score,current);
+
+            for(int j=0;j<s.length();j++) {
+                int x=s.charAt(j)-'a';
+                freq[x]++;
+                current-=score[x];
+            }
         }
-        
+    }
+
+    public int maxScoreWords(String[] words, char[] letters, int[] score) {
+        int[] freq=new int[26];
+
+        for(char c:letters)
+            freq[c-'a']++;
+
+        dfs(0,words.length,words,freq,score,0);
+
         return ans;
     }
 }
